a4b98784909a5848a65d065223b955d2dcfb5be9
Grzegorz Czarnocki <33869203+nc-gcz@users.noreply.github.com>
2024-02-22 10:44:00 +0100

    Update ShortcutCreate.ps1
    Rename a shortcut

--- ShortcutCreate.ps1
+++ ShortcutCreate.ps1
@@ -1,10 +1,9 @@
 #Requires -RunAsAdministrator
 param([String]$workDir=$pwd)
 
-$name = "UFST VPN proxy"
+$name = "UFST VPN Proxy"
 
 $startName = $name + " Start"
-
 
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\$startName.lnk")
@@ -12,4 +11,3 @@ $Shortcut.IconLocation = "$workDir\vpn.ico"
 $Shortcut.TargetPath = "$workDir\start.exe"
 $Shortcut.WorkingDirectory = $workDir
 $Shortcut.Save()
-
